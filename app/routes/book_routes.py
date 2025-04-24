b9783b12a3e1291bb36dacf80ed5699e642e9ab3
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-23 19:08:03 -0700

    commented out old code (prep for lesson 03 onward)

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,41 +1,41 @@
 from flask import Blueprint, abort, make_response
-from app.models.book import books
+# from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 
-@books_bp.get("")
-def get_all_books():
+# @books_bp.get("")
+# def get_all_books():
 
-    return [
-            {
-            "id": book.id,
-            "title": book.title,
-            "description": book.description,
-            } for book in books]
-
-
-@books_bp.get("/<book_id>")
-def get_one_book(book_id):
-    book = validate_book(book_id)
-
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
-    }
+#     return [
+#             {
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description,
+#             } for book in books]
 
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except ValueError:
-        response = {"message": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
+# @books_bp.get("/<book_id>")
+# def get_one_book(book_id):
+#     book = validate_book(book_id)
 
-    for book in books:
-        if book.id == book_id:
-            return book
+#     return {
+#         "id": book.id,
+#         "title": book.title,
+#         "description": book.description,
+#     }
 
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
+
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except ValueError:
+#         response = {"message": f"book {book_id} invalid"}
+#         abort(make_response(response, 400))
+
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+
+#     response = {"message": f"book {book_id} not found"}
+#     abort(make_response(response, 404))
