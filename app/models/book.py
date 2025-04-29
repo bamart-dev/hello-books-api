32d989a99ca38885db6611a359821e0d9a635adb
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-28 22:36:54 -0700

    setup connections to database

--- app/models/book.py
+++ app/models/book.py
@@ -1,4 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db import db
 
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
 
 # class Book:
 #     def __init__(self, id, title, description):
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,41 +1,64 @@
-from flask import Blueprint, abort, make_response
-# from app.models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from app.db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+@books_bp.post("", strict_slashes=False)
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
 
-# @books_bp.get("")
-# def get_all_books():
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
 
-#     return [
-#             {
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description,
-#             } for book in books]
+    response = {
+        "id": new_book.id,
+        "title" : new_book.title,
+        "description": new_book.description,
+    }
 
-
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     book = validate_book(book_id)
-
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description,
-#     }
+    return response, 201
 
 
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except ValueError:
-#         response = {"message": f"book {book_id} invalid"}
-#         abort(make_response(response, 400))
+@books_bp.get("", strict_slashes=False)
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
 
-#     for book in books:
-#         if book.id == book_id:
-#             return book
+    return [
+            {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description,
+            } for book in books]
 
-#     response = {"message": f"book {book_id} not found"}
-#     abort(make_response(response, 404))
+
+@books_bp.get("/<book_id>", strict_slashes=False)
+def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except ValueError:
+        response = {"message": f"book ({book_id}) invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    if not book:
+        response = {"message": f"book ({book_id}) not found"}
+        abort(make_response(response, 404))
+
+    return book
